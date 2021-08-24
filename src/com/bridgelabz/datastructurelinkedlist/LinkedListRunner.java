21ae0df6b17e6f3a364b663ecdb4f8aebc481eec
Prakash-16 <goudprakash58@gmail.com>
2021-08-24 17:08:22 +0530

    [Prakash] - Created a simple linked list

---
+++ src/com/bridgelabz/datastructurelinkedlist/LinkedListRunner.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.datastructurelinkedlist;
+
+public class LinkedListRunner {
+	
+	public static void main(String[] args) {
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+		System.out.println(result);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module DataStructureLinkedList {
+}
